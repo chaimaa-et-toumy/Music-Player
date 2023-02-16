59472f1d949e83d45615b520f5512ae8b0350545
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2023-02-16 14:08:56 +0100

    add initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App test eeei</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
